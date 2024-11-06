d5232840db2b4eb7e7cbab8d97d35e2b24a797aa
Miraç Akkoyun <miracakkoyun00@gmail.com>
2024-11-07 00:44:31 +0300

    list

--- src/newPkg/LinkedList.java
+++ src/newPkg/LinkedList.java
@@ -31,6 +31,119 @@ public class LinkedList {
 			System.out.println("Sona eleman eklendi");
 		}
 	}
+	void arayaEkle(int index,int x) {
+		Node eleman=new Node();
+		eleman.data=x;
+		if(head==null && index==0) {
+			head=eleman;
+			tail=eleman;
+			System.out.println("Liste yapısı oluşturuldu ilk eleman eklendi");
+		}else if(head!=null && index==0) {
+			eleman.next=head;
+			head=eleman;
+			System.out.println(index+" indisinci sıraya eleman eklendi");
+		}
+		else {
+			int n=0;
+			Node temp=head;
+			Node temp2=head;
+			while(temp.next!=null) {
+				n++;
+				temp2=temp;
+				temp=temp.next;
+				
+			}
+			if(n==index ) {
+				temp2.next=eleman;
+				eleman.next=temp;
+				
+				System.out.println(index+" indisinci sıraya eleman eklendi");
+			}else {
+				temp=head;
+				temp2=head;
+				int i=0;
+				while(i!=index) {
+					temp2=temp;
+					temp=temp.next;
+					i++;
+					
+				}
+				temp2.next=eleman;
+				eleman.next=temp;
+				System.out.println(index+" indisinci sıraya eleman eklendi");
+
+			}
+			
+		}
+	}
+	void bastanSil() {
+		if(head==null) {
+			System.out.println("Liste boş silinecek eleman yok");
+		}else if(head.next==null) {
+			head=null;
+			tail=null;
+			System.out.println("Listede kalan son eleman silindi");
+		}else {
+			head=head.next;
+			System.out.println("Baştaki eleman silindi");
+			
+		}
+	}
+	void sondanSil() {
+		if(head==null) {
+			System.out.println("Liste boş silinecek eleman yok");
+		}else if(head.next==null) {
+			head=null;
+			tail=null;
+			System.out.println("Listede kalan son eleman silindi");
+		}else {
+			Node temp=head;
+			Node temp2=head;
+			while(temp.next!=null) {
+				temp2=temp;
+				temp=temp.next;
+			}
+			temp2.next=null;
+			tail=temp2;
+			System.out.println("Sondaki eleman silindi");
+		}
+	}
+	void aradanSil(int index) {
+		if(head==null) {
+			System.out.println("Liste boş silinecek eleman yok");
+		}else if(head.next==null && index==0) {
+			head=null;
+			tail=null;
+			System.out.println("Listede kalan son eleman silindi");
+		}else if(head.next!=null && index==0) {
+			head=head.next;
+			System.out.println("Baştaki eleman silindi");
+		}else {
+			int n=0;
+			Node temp=head;
+			Node temp2=head;
+			while(temp.next!=null) {
+				temp2=temp;
+				temp=temp.next;
+				n++;
+			}
+			if(n==index) {
+				temp2.next=null;
+				tail=temp2;
+			}else {
+				temp=head;
+				temp2=head;
+				int j=0;
+				while(j!=index) {
+					temp2=temp;
+					temp=temp.next;
+					j++;
+				}
+				temp2.next=temp.next;
+				System.out.println("Aradaki eleman silindi");
+			}
+		}
+	}
 	void yazdir() {
 		if(head==null) {
 			System.out.println("Liste boş yazdırılacak eleman yok");
--- src/newPkg/Main.java
+++ src/newPkg/Main.java
@@ -1,10 +1,87 @@
 package newPkg;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		LinkedList sayi=new LinkedList();
-		sayi.yazdir();
+		LinkedList liste = new LinkedList();
+		int secim =12;
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		int sayi,index;
+		while(secim!=0) {
+			System.out.println("1 basa ekle ");
+			System.out.println("2 sona ekle ");
+			System.out.println("3 araya ekle ");
+			System.out.println("4 bastan sil ");
+			System.out.println("5 sondan sil");
+			System.out.println("6 aradan sil ");
+			System.out.println("7 listeyi yazdır");
+			System.out.println("0 çıkış ");
+			System.out.println("Seçiminiz: ");
+			secim=scanner.nextInt();
+			if(secim==1) {
+				System.out.print("Sayı: ");sayi=scanner.nextInt();System.out.println();
+				liste.basaEkle(sayi);
+			}else if(secim==2) {
+				System.out.print("Sayı: ");sayi=scanner.nextInt();System.out.println();
+				liste.sonaEkle(sayi);
+			}else if(secim==3) {
+				System.out.print("İndex: ");index=scanner.nextInt();System.out.println();
+
+				System.out.print("Sayı: ");sayi=scanner.nextInt();System.out.println();
+				liste.arayaEkle(index, sayi);
+
+			}else if(secim==4) {
+				liste.bastanSil();
+			}else if(secim==5) {
+				liste.sondanSil();
+			}else if(secim==6) {
+				System.out.print("İndex: ");index=scanner.nextInt();System.out.println();
+
+				liste.aradanSil(index);
+			}else if(secim==7) {
+				liste.yazdir();
+			}else if(secim==0) {
+				System.out.println("Programdan çıkılıyor iyi günler");
+			}else {
+				System.out.println("Hatalı seçim lütfen tekrar giriniz");
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*sayi.yazdir();
 		sayi.sonaEkle(100);
 		sayi.basaEkle(0);
 		sayi.basaEkle(12);
@@ -12,6 +89,30 @@ public class Main {
 		sayi.sonaEkle(33);
 		sayi.basaEkle(39);
 		sayi.yazdir();
+		sayi.arayaEkle(0, 5);
+		sayi.yazdir();
+		sayi.arayaEkle(2, 150);
+		sayi.yazdir();
+		sayi.arayaEkle(7, 260);
+		sayi.yazdir();
+		sayi.arayaEkle(9, 1000);
+		sayi.yazdir();
+		sayi.bastanSil();
+		sayi.yazdir();
+
+		sayi.bastanSil();
+		sayi.bastanSil();
+		sayi.bastanSil();
+		sayi.yazdir();
+		sayi.sondanSil();
+		sayi.yazdir();
+		sayi.aradanSil(0);
+		sayi.yazdir();
+		sayi.aradanSil(1);
+		sayi.yazdir();
+		sayi.aradanSil(2);
+		sayi.yazdir();*/
+
 	}
 
 }
